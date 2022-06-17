f5ff1985708d012c37fe266e409ac3c0d4c9b26c
Rajat <remixrajat1@gmail.com>
2022-06-17 18:56:04 +0530

    adding code with error

--- dist/routes/todos.js
+++ dist/routes/todos.js
@@ -16,57 +16,112 @@ const express_1 = require("express");
 const database_1 = __importDefault(require("../database/database"));
 const router = (0, express_1.Router)();
 router.get("/", (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
-    database_1.default.query("select * from todos_table", (error, results, fields) => {
-        if (error)
-            res.status(400).json({ message: error.message });
-        else
-            res.status(200).json({ data: results });
-    });
+    try {
+        console.log("Hello");
+        const result = yield database_1.default.query(`select * from todos_table`);
+        console.log(result);
+        // res.status(200).json({ data: result });
+    }
+    catch (error) {
+        console.log(error);
+        res.status(400).json({ message: error.message });
+    }
+    // db.query(
+    //   "select * from todos_table",
+    //   (error: any, results: any, fields: any) => {
+    //     if (error) res.status(400).json({ message: error.message });
+    //     else res.status(200).json({ data: results });
+    //   }
+    // );
 }));
-router.post("/todo", (req, res, next) => {
+router.post("/todo", (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     const body = req.body;
-    console.log(body);
-    database_1.default.query(`insert into todos_table(todo) values("${body.todo}")`, (error, results, fields) => {
-        if (error) {
-            console.log(error);
-            return res.status(400).json({ message: error.message });
-        }
-        res.status(200).json({ data: results });
-    });
-});
-router.put("/todo/:todoId", (req, res, next) => {
+    try {
+        const result = yield database_1.default.query(`insert into todos_table(todo) values("${body.todo}")`);
+        res.status(200).json({ data: result });
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+    // db.query(
+    //   `insert into todos_table(todo) values("${body.todo}")`,
+    //   (error: any, results: any, fields: any) => {
+    //     if (error) {
+    //       console.log(error);
+    //       return res.status(400).json({ message: error.message });
+    //     }
+    //     res.status(200).json({ data: results });
+    //   }
+    // );
+}));
+router.put("/todo/:todoId", (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     const body = req.body;
     const params = req.params;
     const tid = Number(params.todoId);
-    console.log(tid, typeof tid);
-    database_1.default.query(`select todo_id, todo from todos_table where todo_id=${tid}`, (error, results, fields) => {
-        if (error)
-            res.status(400).json({ message: error.message });
-        else {
-            console.log(results);
-            if (results.length > 0) {
-                database_1.default.query(`update todos_table set todo="${body.todo}" where todo_id=${tid}`, (error, results, fields) => {
-                    if (error)
-                        res.status(400).json({ message: error.message });
-                    else
-                        res.status(200).json({
-                            message: "Successfully updated todo.",
-                            results: results,
-                        });
-                });
-            }
+    try {
+        try {
+            var data = yield database_1.default.query(`select todo_id, todo from todos_table where todo_id=${tid}`);
         }
-    });
-});
-router.delete("/todo/:todoId", (req, res, next) => {
-    const params = req.params;
-    database_1.default.query(`delete from todos_table where todo_id=${Number(params.todoId)}`, (error, results, fields) => {
-        if (error)
-            res.status(400).json({ message: error.message });
-        else
+        catch (err) {
+            res.status(400).json({ message: err.message });
+        }
+        if (data.length > 0) {
+            const result = yield database_1.default.query(`update todos_table set todo="${body.todo}" where todo_id=${tid}`);
+            res.status(200).json({
+                message: "Successfully updated todo.",
+                results: result,
+            });
+        }
+        else {
             res
-                .status(200)
-                .json({ message: "Successfully Deleted.", data: results });
-    });
-});
+                .json(200)
+                .json({ message: "Success", response: "No data found for this id" });
+        }
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+    // db.query(
+    //   `select todo_id, todo from todos_table where todo_id=${tid}`,
+    //   (error: any, results: any, fields: any) => {
+    //     if (error) res.status(400).json({ message: error.message });
+    //     else {
+    //       console.log(results);
+    //       if (results.length > 0) {
+    //         db.query(
+    //           `update todos_table set todo="${body.todo}" where todo_id=${tid}`,
+    //           (error: any, results: any, fields: any) => {
+    //             if (error) res.status(400).json({ message: error.message });
+    //             else
+    //               res.status(200).json({
+    //                 message: "Successfully updated todo.",
+    //                 results: results,
+    //               });
+    //           }
+    //         );
+    //       }
+    //     }
+    //   }
+    // );
+}));
+router.delete("/todo/:todoId", (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const params = req.params;
+    try {
+        const result = yield database_1.default.query(`delete from todos_table where todo_id=${Number(params.todoId)}`);
+        res.status(200).json({ message: "Successfully Deleted.", data: result });
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+    // db.query(
+    //   `delete from todos_table where todo_id=${Number(params.todoId)}`,
+    //   (error: any, results: any, fields: any) => {
+    //     if (error) res.status(400).json({ message: error.message });
+    //     else
+    //       res
+    //         .status(200)
+    //         .json({ message: "Successfully Deleted.", data: results });
+    //   }
+    // );
+}));
 exports.default = router;
--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -7,72 +7,123 @@ type RequestBody = { todo: string };
 type RequestParams = { todoId: string };
 
 router.get("/", async (req: any, res: any, next) => {
-  db.query(
-    "select * from todos_table",
-    (error: any, results: any, fields: any) => {
-      if (error) res.status(400).json({ message: error.message });
-      else res.status(200).json({ data: results });
-    }
-  );
+  try {
+    console.log("Hello");
+    const result = await db.query(`select * from todos_table`);
+    console.log(result);
+    // res.status(200).json({ data: result });
+  } catch (error: any) {
+    console.log(error);
+    res.status(400).json({ message: error.message });
+  }
+
+  // db.query(
+  //   "select * from todos_table",
+  //   (error: any, results: any, fields: any) => {
+  //     if (error) res.status(400).json({ message: error.message });
+  //     else res.status(200).json({ data: results });
+  //   }
+  // );
 });
 
-router.post("/todo", (req, res, next) => {
+router.post("/todo", async (req, res, next) => {
   const body = req.body as RequestBody;
-  console.log(body);
-  db.query(
-    `insert into todos_table(todo) values("${body.todo}")`,
-    (error: any, results: any, fields: any) => {
-      if (error) {
-        console.log(error);
-        return res.status(400).json({ message: error.message });
-      }
 
-      res.status(200).json({ data: results });
-    }
-  );
+  try {
+    const result = await db.query(
+      `insert into todos_table(todo) values("${body.todo}")`
+    );
+    res.status(200).json({ data: result });
+  } catch (error: any) {
+    res.status(400).json({ message: error.message });
+  }
+  // db.query(
+  //   `insert into todos_table(todo) values("${body.todo}")`,
+  //   (error: any, results: any, fields: any) => {
+  //     if (error) {
+  //       console.log(error);
+  //       return res.status(400).json({ message: error.message });
+  //     }
+
+  //     res.status(200).json({ data: results });
+  //   }
+  // );
 });
 
-router.put("/todo/:todoId", (req, res, next) => {
+router.put("/todo/:todoId", async (req, res, next) => {
   const body = req.body as RequestBody;
   const params = req.params as RequestParams;
   const tid = Number(params.todoId);
-  console.log(tid, typeof tid);
-
-  db.query(
-    `select todo_id, todo from todos_table where todo_id=${tid}`,
-    (error: any, results: any, fields: any) => {
-      if (error) res.status(400).json({ message: error.message });
-      else {
-        console.log(results);
-        if (results.length > 0) {
-          db.query(
-            `update todos_table set todo="${body.todo}" where todo_id=${tid}`,
-            (error: any, results: any, fields: any) => {
-              if (error) res.status(400).json({ message: error.message });
-              else
-                res.status(200).json({
-                  message: "Successfully updated todo.",
-                  results: results,
-                });
-            }
-          );
-        }
-      }
+  try {
+    try {
+      var data = await db.query(
+        `select todo_id, todo from todos_table where todo_id=${tid}`
+      );
+    } catch (err: any) {
+      res.status(400).json({ message: err.message });
     }
-  );
+
+    if (data.length > 0) {
+      const result = await db.query(
+        `update todos_table set todo="${body.todo}" where todo_id=${tid}`
+      );
+      res.status(200).json({
+        message: "Successfully updated todo.",
+        results: result,
+      });
+    } else {
+      res
+        .json(200)
+        .json({ message: "Success", response: "No data found for this id" });
+    }
+  } catch (error: any) {
+    res.status(400).json({ message: error.message });
+  }
+
+  // db.query(
+  //   `select todo_id, todo from todos_table where todo_id=${tid}`,
+  //   (error: any, results: any, fields: any) => {
+  //     if (error) res.status(400).json({ message: error.message });
+  //     else {
+  //       console.log(results);
+  //       if (results.length > 0) {
+  //         db.query(
+  //           `update todos_table set todo="${body.todo}" where todo_id=${tid}`,
+  //           (error: any, results: any, fields: any) => {
+  //             if (error) res.status(400).json({ message: error.message });
+  //             else
+  //               res.status(200).json({
+  //                 message: "Successfully updated todo.",
+  //                 results: results,
+  //               });
+  //           }
+  //         );
+  //       }
+  //     }
+  //   }
+  // );
 });
 
-router.delete("/todo/:todoId", (req, res, next) => {
+router.delete("/todo/:todoId", async (req, res, next) => {
   const params = req.params as RequestParams;
-  db.query(
-    `delete from todos_table where todo_id=${Number(params.todoId)}`,
-    (error: any, results: any, fields: any) => {
-      if (error) res.status(400).json({ message: error.message });
-      else
-        res
-          .status(200)
-          .json({ message: "Successfully Deleted.", data: results });
-    }
-  );
+
+  try {
+    const result = await db.query(
+      `delete from todos_table where todo_id=${Number(params.todoId)}`
+    );
+    res.status(200).json({ message: "Successfully Deleted.", data: result });
+  } catch (error: any) {
+    res.status(400).json({ message: error.message });
+  }
+  // db.query(
+  //   `delete from todos_table where todo_id=${Number(params.todoId)}`,
+  //   (error: any, results: any, fields: any) => {
+  //     if (error) res.status(400).json({ message: error.message });
+  //     else
+  //       res
+  //         .status(200)
+  //         .json({ message: "Successfully Deleted.", data: results });
+  //   }
+  // );
 });
 export default router;
